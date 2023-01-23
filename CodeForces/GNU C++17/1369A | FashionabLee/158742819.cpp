8c8c097c8200cd6f024487477488f8be8d4c370a
Mohammed Islam <61063541+PIslam19@users.noreply.github.com>
2023-01-24 04:59:51 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1369A | FashionabLee/158742819.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define fast ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long int
+#define vi vector<int>
+#define fr(a) for(int i=0;i<a;i++)
+#define frr(a) for (int i=1;i<=a;i++)
+using namespace std;
+void solve()
+{
+    ll n;
+    cin>>n;
+    if(n%4==0)cout<< "Yes"<<endl;
+    else cout<< "NO"<<endl;
+}
+int main()
+{
+    fast;
+    int test;
+    cin>>test;
+    fr(test)
+   {
+        solve();
+   }
+ 
+    return 0;
+}
