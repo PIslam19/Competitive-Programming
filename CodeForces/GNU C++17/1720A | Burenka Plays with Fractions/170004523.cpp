038322e110ffd5ed2ce6103a363d01c35493bc54
Mohammed Islam <61063541+PIslam19@users.noreply.github.com>
2023-01-24 04:58:32 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1720A | Burenka Plays with Fractions/170004523.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define fast ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long int
+#define vi vector<int>
+#define fr(a) for(int i=0;i<a;i++)
+#define frr(a) for (int i=1;i<=a;i++)
+using namespace std;
+void solve()
+{
+    ll a,b,c,d;
+    cin>>a>>b>>c>>d;
+    ll x=a*d;
+    ll y=b*c;
+    if(x==y)
+    {
+        cout<<0<<endl;
+    }
+    else if(x==0 ||y==0)
+    {
+        cout<<1<<endl;
+    }
+    else if(x%y==0 || y%x==0)
+    {
+        cout<<1<<endl;
+    }
+    else
+    {
+        cout<<2<<endl;
+    }
+}
+int main()
+{
+    fast;
+    int test;
+    cin>>test;
+    fr(test)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
