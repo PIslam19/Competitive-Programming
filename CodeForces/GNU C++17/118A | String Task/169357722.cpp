7b534081345f53c533c693a19bc2f6e3a322c172
Mohammed Islam <61063541+PIslam19@users.noreply.github.com>
2023-01-24 04:59:10 +0600

    Committing files

---
+++ CodeForces/GNU C++17/118A | String Task/169357722.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define fast ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long int
+#define vi vector<int>
+#define fr(a) for(int i=0;i<a;i++)
+#define frr(a) for (int i=1;i<=a;i++)
+using namespace std;
+void solve()
+{
+string s;
+cin>>s;
+transform(s.begin(),s.end(),s.begin(),::tolower);
+//cout<<s;
+ll n=s.size();
+fr(n)
+{
+    if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||s[i]=='y')
+    {
+        continue;
+    }
+    else
+    {
+        cout<<"."<<s[i];
+    }
+}
+}
+int main()
+{
+    fast;
+    int test=1;
+    //cin>>test;
+    fr(test)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
