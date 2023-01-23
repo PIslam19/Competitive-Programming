a2fe0952f1ac206f61de8f6ef76c1c257c6026e6
Mohammed Islam <61063541+PIslam19@users.noreply.github.com>
2023-01-24 04:56:54 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1758B | XOR = Average/182585901.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#define fast ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long int
+#define vi vector<int>
+#define fr(a) for(int i=0;i<a;i++)
+#define frr(a) for (int i=1;i<=a;i++)
+using namespace std;
+ 
+ 
+void solve()
+{
+    ll n;
+    cin>>n;
+    if(n%2==1)
+    {
+        fr(n)
+        {
+          cout<< 1<< " ";  
+        }
+        cout<<endl;
+        
+    }
+    else
+    {
+        fr(n-2)
+        {
+            cout<<2<< " ";
+        }
+        cout<< "1 3"<<endl;
+    }
+ 
+ 
+}
+ 
+int main()
+{
+    fast;
+    int test;
+    cin>>test;
+    fr(test)
+    {
+        solve();
+    }
+    return 0;
+}
