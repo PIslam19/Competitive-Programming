d40b108460e8db38e3819daff3f94dae30fd3263
Mohammed Islam <61063541+PIslam19@users.noreply.github.com>
2023-01-24 04:59:16 +0600

    Committing files

---
+++ CodeForces/GNU C++17/112A | Petya and Strings/169354691.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define fast ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long int
+#define vi vector<int>
+#define fr(a) for(int i=0;i<a;i++)
+#define frr(a) for (int i=1;i<=a;i++)
+using namespace std;
+void solve()
+{
+string s,ss;
+cin>>s>>ss;
+ transform(s.begin(), s.end(), s.begin(), ::toupper);
+ transform(ss.begin(), ss.end(), ss.begin(), ::toupper);
+ if(s==ss)
+ {
+     cout<<0<<endl;
+ }
+ else if(s>ss)
+ {
+     cout<<1<<endl;
+ }
+ else{
+    cout<<-1<<endl;
+ }
+}
+int main()
+{
+    fast;
+    int test=1;
+    //cin>>test;
+    fr(test)
+    {
+        solve();
+    }
+ 
+    return 0;
+}
