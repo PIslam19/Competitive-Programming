f4043b2ef8f32c7c3b61caa852040092a95aa982
Mohammed Islam <61063541+PIslam19@users.noreply.github.com>
2023-01-24 04:56:38 +0600

    Committing files

---
+++ CodeForces/GNU C++17/554A | Kyoya and Photobooks/183339075.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#define fast ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long int
+#define vi vector<int>
+#define fr(a) for(int i=0;i<a;i++)
+#define fr2(a) for(int j=0;j<a;j++)
+#define frr(a) for (int i=1;i<=a;i++)
+#define frr2(a) for (int j=1;j<=a;j++)
+#define max INT_MAX;
+using namespace std;
+ 
+ 
+void solve()
+{
+    string ss;
+    cin>>ss;
+    ll n=ss.size();
+  
+  cout<<26+(25*n)<<endl;
+ 
+}
+ 
+int main()
+{
+    fast;
+    int test=1;
+    //cin>>test;
+    fr(test)
+    {
+        solve();
+    }
+    return 0;
+}
